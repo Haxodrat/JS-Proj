f546cf5f4f78f56c69766f41d72bbe89a230a377
Christopher Kim <haxodrat@icloud.com>
2024-06-15 16:20:10 -0700

    readme

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # JS-Proj
 
 This repository is just a reference for JavaScript-related tips and syntax pertinent to future web application development.
+
+## Tech Stack
+
+### Languages:
+
+![JavaScript](https://img.shields.io/badge/-JavaScript-%23F7DF1C?style=flat-square&logo=javascript&logoColor=000000&labelColor=%23F7DF1C&color=%23FFCE5A)
+![HTML5](https://img.shields.io/badge/-HTML5-%23E44D27?style=flat-square&logo=html5&logoColor=ffffff)
+![CSS3](https://img.shields.io/badge/-CSS3-%231572B6?style=flat-square&logo=css3)
--- sandbox.js
+++ sandbox.js
@@ -1,213 +1,215 @@
 
-const content = document.querySelectorAll('p');
-
-console.log(content);
-
-content.forEach(tag => {
-    if (tag.textContent.includes('error')) {
-        tag.classList.add('error');
-    }
-    if (tag.textContent.includes('success')) {
-        tag.classList.add('success');
-    }   
-});
-
-console.log(content);
-
-const title = document.querySelector('.title');
-
-title.classList.toggle('test');
-title.classList.toggle('test');
 
 
-//innerText gets visible text while textContent gets all possible text in code
+// const content = document.querySelectorAll('p');
 
-// const content = document.querySelector('p');
+// console.log(content);
 
-// console.log(content.classList);
-// content.classList.add('error');
-// content.classList.remove('error');
-// content.classList.add('success');
+// content.forEach(tag => {
+//     if (tag.textContent.includes('error')) {
+//         tag.classList.add('error');
+//     }
+//     if (tag.textContent.includes('success')) {
+//         tag.classList.add('success');
+//     }   
+// });
 
-// // const title = document.querySelector('h1');
+// console.log(content);
 
-// // //overwrites the css properties in h1 tag
-// // // title.setAttribute("style", "margin: 50px;");
+// const title = document.querySelector('.title');
 
-// // console.log(title.style);
-// // console.log(title.style.color);
+// title.classList.toggle('test');
+// title.classList.toggle('test');
 
-// // title.style.margin = '50px';
-// // title.style.color = 'crimson';
-// // title.style.fontSize = '60px';
-// // title.style.margin = '';
 
-// // // const link = document.querySelector('a');
+// //innerText gets visible text while textContent gets all possible text in code
 
-// // // console.log(link.getAttribute('href'));
-// // // link.setAttribute('href', 'https://www.thenetninja.co.uk');
-// // // link.innerText = 'The Net Ninja Website';
+// // const content = document.querySelector('p');
 
-// // // const msg = document.querySelector('p');
+// // console.log(content.classList);
+// // content.classList.add('error');
+// // content.classList.remove('error');
+// // content.classList.add('success');
 
-// // // console.log(msg.getAttribute('class'));
-// // // msg.setAttribute('class', 'success');
-// // // msg.setAttribute('style', 'color: green;');
+// // // const title = document.querySelector('h1');
 
-// // // // const para = document.querySelector('p');
+// // // //overwrites the css properties in h1 tag
+// // // // title.setAttribute("style", "margin: 50px;");
 
-// // // // // console.log(para.innerText);
-// // // // // para.innerText = 'ninjas are awesome';
+// // // console.log(title.style);
+// // // console.log(title.style.color);
 
-// // // // const paras = document.querySelectorAll('p');
-// // // // // paras.forEach(para => {
-// // // // //     console.log(para.innerText);
-// // // // //     para.innerText += ' new text';
+// // // title.style.margin = '50px';
+// // // title.style.color = 'crimson';
+// // // title.style.fontSize = '60px';
+// // // title.style.margin = '';
+
+// // // // const link = document.querySelector('a');
+
+// // // // console.log(link.getAttribute('href'));
+// // // // link.setAttribute('href', 'https://www.thenetninja.co.uk');
+// // // // link.innerText = 'The Net Ninja Website';
+
+// // // // const msg = document.querySelector('p');
+
+// // // // console.log(msg.getAttribute('class'));
+// // // // msg.setAttribute('class', 'success');
+// // // // msg.setAttribute('style', 'color: green;');
+
+// // // // // const para = document.querySelector('p');
+
+// // // // // // console.log(para.innerText);
+// // // // // // para.innerText = 'ninjas are awesome';
+
+// // // // // const paras = document.querySelectorAll('p');
+// // // // // // paras.forEach(para => {
+// // // // // //     console.log(para.innerText);
+// // // // // //     para.innerText += ' new text';
+// // // // // // });
+
+// // // // // const content = document.querySelector('.content');
+
+// // // // // // console.log(content.innerHTML);
+// // // // // // content.innerHTML = '<h2>This is a new h2</h2>';
+
+// // // // // const people = ["mario", "yoshi", "luigi"];
+
+// // // // // people.forEach(person => {
+// // // // //     content.innerHTML += `<p>${person}</p>`;
 // // // // // });
 
-// // // // const content = document.querySelector('.content');
 
-// // // // // console.log(content.innerHTML);
-// // // // // content.innerHTML = '<h2>This is a new h2</h2>';
+// // // // // // const paras = document.getElementsByTagName('p');
+// // // // // // console.log(paras);
 
-// // // // const people = ["mario", "yoshi", "luigi"];
+// // // // // // // const title = document.getElementById("page-title");
+// // // // // // // console.log(title);
 
-// // // // people.forEach(person => {
-// // // //     content.innerHTML += `<p>${person}</p>`;
-// // // // });
-
-
-// // // // // const paras = document.getElementsByTagName('p');
-// // // // // console.log(paras);
-
-// // // // // // const title = document.getElementById("page-title");
-// // // // // // console.log(title);
-
-// // // // // // const errors = document.getElementsByClassName('error');
-// // // // // // console.log(errors);
-// // // // // // //can't use forEach loop on HTML collection
-
-// // // // // // // const paras = document.querySelectorAll('p');
-// // // // // // // const errors = document.querySelectorAll('.error');
-
+// // // // // // // const errors = document.getElementsByClassName('error');
 // // // // // // // console.log(errors);
+// // // // // // // //can't use forEach loop on HTML collection
 
-// // // // // // // const para = document.querySelector('body > h1');
+// // // // // // // // const paras = document.querySelectorAll('p');
+// // // // // // // // const errors = document.querySelectorAll('.error');
 
-// // // // // // // console.log(para);
+// // // // // // // // console.log(errors);
 
-// // // // // // // let user = {
-// // // // // // //     name: 'crystal',
-// // // // // // //     age: 30,
-// // // // // // //     email: 'crystal@gmail.com',
-// // // // // // //     location: 'berlin',
-// // // // // // //     blogs: [
-// // // // // // //         { title: 'why mac & cheese', likes: 30 },
-// // // // // // //         { title: 'wh', likes: 2 }
-// // // // // // //     ],
-// // // // // // //     login() {
-// // // // // // //         console.log('the user logged in.');
-// // // // // // //     },
-// // // // // // //     logout() {
-// // // // // // //         console.log('the user logged out.');
-// // // // // // //     },
-// // // // // // //     logBlogs() {
-// // // // // // //         console.log('this user has written the following blogs: ');
-// // // // // // //         this.blogs.forEach(blog => {
-// // // // // // //             console.log(blog.title, blog.likes);
-// // // // // // //         })
-// // // // // // //     }
-// // // // // // // };
+// // // // // // // // const para = document.querySelector('body > h1');
 
-// // // // // // // user.login();
-// // // // // // // user.logBlogs();
+// // // // // // // // console.log(para);
 
-
-
-// // // // // // // const names = ['hi', 'bye'];
-// // // // // // // for(let num = 0; num < names.length; num++) {
-// // // // // // //     let html = `<div>${names[num]}</div>`;
-// // // // // // //     console.log(html);
-// // // // // // // }
-
-// // // // // // // let i = 5;
-// // // // // // // do {
-// // // // // // //     console.log('val of i is: ', i);
-// // // // // // //     i++;
-// // // // // // // }
-// // // // // // // while (i < 5);
-
-
-// // // // // // // const calcArea = radius => 3.14 * radius ** 2;
-
-// // // // // // // const area = calcArea(5);
-// // // // // // // console.log('area is: ', area);
-
-// // // // // // // const myFunc = (callbackFunc) => {
-// // // // // // //     let value = 50;
-// // // // // // //     callbackFunc(value);
-// // // // // // // };
-
-// // // // // // // myFunc(value => {
-// // // // // // //     console.log(value);
-// // // // // // // });
-
-// // // // // // // const ul = document.querySelector('.people');
-
-// // // // // // // const people = ['bob', 'luigi', 'ryu'];
-
-// // // // // // // let html = ``;
-
-// // // // // // // people.forEach(person => {
-// // // // // // //     html += `<li style="color: purple">${person}</li>`;
-// // // // // // // });
-
-// // // // // // // console.log(html);
-// // // // // // // ul.innerHTML = html;
-// // // // // // // // let people = ['bob', 'knox', 'greg'];
-
-// // // // // // // // const logPerson = (person, index) => {
-// // // // // // // //     console.log(`${index} - hello ${person}`);
+// // // // // // // // let user = {
+// // // // // // // //     name: 'crystal',
+// // // // // // // //     age: 30,
+// // // // // // // //     email: 'crystal@gmail.com',
+// // // // // // // //     location: 'berlin',
+// // // // // // // //     blogs: [
+// // // // // // // //         { title: 'why mac & cheese', likes: 30 },
+// // // // // // // //         { title: 'wh', likes: 2 }
+// // // // // // // //     ],
+// // // // // // // //     login() {
+// // // // // // // //         console.log('the user logged in.');
+// // // // // // // //     },
+// // // // // // // //     logout() {
+// // // // // // // //         console.log('the user logged out.');
+// // // // // // // //     },
+// // // // // // // //     logBlogs() {
+// // // // // // // //         console.log('this user has written the following blogs: ');
+// // // // // // // //         this.blogs.forEach(blog => {
+// // // // // // // //             console.log(blog.title, blog.likes);
+// // // // // // // //         })
+// // // // // // // //     }
 // // // // // // // // };
 
-// // // // // // // // people.forEach(logPerson);
+// // // // // // // // user.login();
+// // // // // // // // user.logBlogs();
 
-// // // // // // // // let radius = 10;
-// // // // // // // // const pi = 3.14;
-// // // // // // // // const author = 'Kailey';
 
-// // // // // // // // console.log(radius, pi);
 
-// // // // // // // // let result = `The lovergirl ${author} has many fans.`;
-// // // // // // // // console.log(result);
+// // // // // // // // const names = ['hi', 'bye'];
+// // // // // // // // for(let num = 0; num < names.length; num++) {
+// // // // // // // //     let html = `<div>${names[num]}</div>`;
+// // // // // // // //     console.log(html);
+// // // // // // // // }
 
-// // // // // // // // let html = `
-// // // // // // // //     <h2>${author}</h2>
-// // // // // // // //     <p>A lot</p>
-// // // // // // // //     <span>Hello</span>
-// // // // // // // // `;
+// // // // // // // // let i = 5;
+// // // // // // // // do {
+// // // // // // // //     console.log('val of i is: ', i);
+// // // // // // // //     i++;
+// // // // // // // // }
+// // // // // // // // while (i < 5);
+
+
+// // // // // // // // const calcArea = radius => 3.14 * radius ** 2;
+
+// // // // // // // // const area = calcArea(5);
+// // // // // // // // console.log('area is: ', area);
+
+// // // // // // // // const myFunc = (callbackFunc) => {
+// // // // // // // //     let value = 50;
+// // // // // // // //     callbackFunc(value);
+// // // // // // // // };
+
+// // // // // // // // myFunc(value => {
+// // // // // // // //     console.log(value);
+// // // // // // // // });
+
+// // // // // // // // const ul = document.querySelector('.people');
+
+// // // // // // // // const people = ['bob', 'luigi', 'ryu'];
+
+// // // // // // // // let html = ``;
+
+// // // // // // // // people.forEach(person => {
+// // // // // // // //     html += `<li style="color: purple">${person}</li>`;
+// // // // // // // // });
 
 // // // // // // // // console.log(html);
+// // // // // // // // ul.innerHTML = html;
+// // // // // // // // // let people = ['bob', 'knox', 'greg'];
 
-// // // // // // // // let ninjas = ['shawn', 'ryu', 'chun-li'];
-// // // // // // // // console.log(ninjas);
+// // // // // // // // // const logPerson = (person, index) => {
+// // // // // // // // //     console.log(`${index} - hello ${person}`);
+// // // // // // // // // };
 
-// // // // // // // // let product = ninjas.join(' ');
-// // // // // // // // product = ninjas.indexOf('chun-li');
-// // // // // // // // //destructive method
-// // // // // // // // product = ninjas.push('ken');
-// // // // // // // // product = ninjas.pop();
+// // // // // // // // // people.forEach(logPerson);
 
-// // // // // // // // console.log(product);
+// // // // // // // // // let radius = 10;
+// // // // // // // // // const pi = 3.14;
+// // // // // // // // // const author = 'Kailey';
 
-// // // // // // // // let email = 'luigi@netninja.org';
-// // // // // // // // let job = email.includes('l');
+// // // // // // // // // console.log(radius, pi);
 
-// // // // // // // // console.log(job);
+// // // // // // // // // let result = `The lovergirl ${author} has many fans.`;
+// // // // // // // // // console.log(result);
 
-// // // // // // // // console.log(pi === 3.14);
-// // // // // // // // console.log(pi == '3.14');
+// // // // // // // // // let html = `
+// // // // // // // // //     <h2>${author}</h2>
+// // // // // // // // //     <p>A lot</p>
+// // // // // // // // //     <span>Hello</span>
+// // // // // // // // // `;
 
-// // // // // // // // let score = '100';
-// // // // // // // // score = Number('hello');
-// // // // // // // // console.log(score);
+// // // // // // // // // console.log(html);
+
+// // // // // // // // // let ninjas = ['shawn', 'ryu', 'chun-li'];
+// // // // // // // // // console.log(ninjas);
+
+// // // // // // // // // let product = ninjas.join(' ');
+// // // // // // // // // product = ninjas.indexOf('chun-li');
+// // // // // // // // // //destructive method
+// // // // // // // // // product = ninjas.push('ken');
+// // // // // // // // // product = ninjas.pop();
+
+// // // // // // // // // console.log(product);
+
+// // // // // // // // // let email = 'luigi@netninja.org';
+// // // // // // // // // let job = email.includes('l');
+
+// // // // // // // // // console.log(job);
+
+// // // // // // // // // console.log(pi === 3.14);
+// // // // // // // // // console.log(pi == '3.14');
+
+// // // // // // // // // let score = '100';
+// // // // // // // // // score = Number('hello');
+// // // // // // // // // console.log(score);
